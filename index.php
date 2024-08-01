3eaeb202ed1a3ab5e66184ac95f9c6d23449a937
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-08-01 15:56:34 +0530

    feat: design and format login page

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="overlap">
+                <div class="navbar">
+                    <a class="text-wrapper">Home</a>
+                    <a class="text-wrapper">About</a>
+                    <a class="text-wrapper">Contact</a>
+                    <a class="text-wrapper">FAQ</a>
+                </div>
+                <div class="game-mind">
+                    <div class="text-wrapper-2">GAME</div>
+                    <div class="text-wrapper-2">MIND</div>
+                </div>
+                <div class="sign-in">
+                    <a href="signin.php">
+                        <button class="text-wrapper-3">Sign in</button>
+                    </a>
+                </div>
+            </div>
+
+            <div class="overlap-group">
+                <img src="images/LoginPageImage.png" class="clip-online" alt="Login page" />
+                <div class="register">
+                    <a href="register.php">
+                        <button class="text-wrapper-4" >Register</button>
+                    </a>
+                </div>
+                <p class="lets-begin-the">
+                    <span class="span">Lets begin the</span>
+                    <span class="text-wrapper-5">&nbsp;</span>
+                    <span class="span">journey<br />to the </span>
+                    <span class="text-wrapper-6">success</span>
+                </p>
+                <p class="the-road-to-success">
+                    The road to success is paved with focused effort and effective time
+                    management. But let&#39;s face it, staying concentrated for long
+                    stretches can be tough. That&#39;s where the Pomodoro Technique comes
+                    in!
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
