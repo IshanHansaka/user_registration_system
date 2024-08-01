3165b527f88276206677008b1aa3fc2058c80d6a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-08-01 20:54:40 +0530

    chore: change css file directry

--- signup.php
+++ signup.php
@@ -33,7 +33,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="signup.css" />
+  <link rel="stylesheet" href="styles/signup.css" />
   <title>Signup</title>
 </head>
   <body>
